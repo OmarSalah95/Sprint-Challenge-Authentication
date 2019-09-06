6675a929df8741218f0a1ff285af59ee6fa1b8d4
omar salah <42569856+OmarSalah95@users.noreply.github.com>
2019-09-06 08:45:54 -0700

    changes

--- dadjoke-client/src/components/PrivateRoute.js
+++ dadjoke-client/src/components/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "http://localhost:3300";
 
 axios.interceptors.request.use(requestConfig => {
   const token = localStorage.getItem("token");
